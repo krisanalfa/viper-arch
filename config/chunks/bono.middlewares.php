f5ca91995f7187e08b510990d3bb945f059bcf84
Krisan Alfa Timur <krisan47@gmail.com>
2014-09-12 01:15:42 +0700

    New divide

--- config/chunks/bono.middlewares.php
+++ config/chunks/bono.middlewares.php
@@ -4,6 +4,7 @@
 return array(
     // The Middlewares
     'bono.middlewares' => array(
+        '\\Bono\\Middleware\\StaticPageMiddleware',
         '\\KrisanAlfa\\Theme\\Middleware\\NotificationMiddleware',
         '\\Bono\\Middleware\\SessionMiddleware',
         '\\KrisanAlfa\\Kraken\\Middleware\\ControllerMiddleware',
